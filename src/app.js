cb3c6b76384f28d2eb7707d362340df03da7a7ec
Diego0014 <88014955+Diego0014@users.noreply.github.com>
2022-04-13 22:36:34 -0300

    model

--- src/app.js
+++ src/app.js
@@ -1,8 +1,53 @@
-var express = require("express");
-var server = express();
-var bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+const bodyParser = require("body-parser");
 
-var model = {};
+let model = {
+  clients: {},
+  reset: () => {
+    model.clients = {};
+  },
+  addAppointment: (name, obj) => {
+    obj = {
+      ...obj,
+      status: "pending",
+    };
+    if (!model.clients[name]) {
+      model.clients[name] = [obj];
+    } else {
+      model.clients[name].push(obj);
+    }
+  },
+  attend: (name, date) => {
+    model.clients[name].map((e) => {
+      if (e.date === date) e.status = "attended";
+    });
+  },
+  expire: (name, date) => {
+    model.clients[name].map((e) => {
+      if (e.date === date) e.status = "expired";
+    });
+  },
+  cancel: (name, date) => {
+    model.clients[name].map((e) => {
+      if (e.date === date) e.status = "cancelled";
+    });
+  },
+  erase: (name, dateOrStatus) => {
+    model.clients[name].map((e) => {
+      if (e.date === dateOrStatus) model.clients[name].shift(e);
+      if (e.status === dateOrStatus) delete e;
+    });
+  },
+  getAppointments: (name, status)=>{ 
+    return model.clients[name];
+  },
+  getClients:()=>{
+      let arr = [];
+      for (const key in model.clients) arr.push(key);
+        return arr;      
+  }
+};
 
 server.use(bodyParser.json());
 
